3eadc20f4a7c9137ace771e17f3958ce0aa09db6
Rico Sharp <rico.sharp@infinity.lan>
2022-07-02 12:02:15 +1000

    fixed abcnews24 scraper

--- abcnews24.py
+++ abcnews24.py
@@ -8,13 +8,12 @@ url = 'https://www.abc.net.au/news/newschannel/'
 request = requests.get(url)
 html = request.text
 soup = BeautifulSoup(html, 'html.parser')
-abc_script = str(soup.body.script)
+abc_script = str(soup.find_all('script'))
 
 # Get JSON from inside the script elements and convert it into proper JSON format
-abc_json = re.findall('{.+}', abc_script)
-abc_json = abc_json[0]
-abc_json = abc_json.replace('\'', '"')
-abc_json = json.loads(abc_json)
+abc_json = re.findall('window.__API__ = {.+}', abc_script)
+abc_json = re.findall('{.+}', abc_json[0])
+abc_json = json.loads(abc_json[0])
 
-# Get final URL
-abc_m3u8 = abc_json.get('url')
+# Grab m3u8 link 
+abc_m3u8 = abc_json['channelpage']['components'][0]['component']['props']['video']['config']['sources'][0]['file']
